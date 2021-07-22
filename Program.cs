21e21f46175810b5017be3f36d4c0c4e42cdb4ce
Sergey Artemov <sergeyartemov@yandex.ru>
2021-07-22 18:43:35 +0300

    start commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadSinchronizeByLock
+{
+    
+    class Program
+    {
+
+        static int x = 0;
+        static object locker = new object();
+        
+        static void Main(string[] args)
+        {
+            for(int i=0; i<=5; i++)
+            {
+                Thread t = new Thread(Count);
+                t.Name = "Поток:" + i.ToString();
+                t.Start();
+
+            }
+
+            Console.ReadLine();
+        }
+    
+        public static void Count()
+        {
+            lock(locker)
+            {
+                x = 1;
+                for(int i = 1; i <= 9; i++)
+                {
+                    Console.WriteLine("{0}:{1}", Thread.CurrentThread.Name,x);
+                    x++;
+                    Thread.Sleep(1000);
+                }
+
+            }
+        }
+    
+    }
+}
